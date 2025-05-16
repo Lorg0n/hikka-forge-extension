7c01f0fb2b4f6d36df87c3fa3f82ae6f7f33dcca
Lorg0n <lorgon.kv@gmail.com>
2025-05-16 20:58:14 +0300

    feat: add clickable to badge

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+export const CONTENT_TYPE_URL = {
+    "anime": "anime",
+    "manga": "manga",
+    "novel": "novel",
+    "collection": "collections",
+    "article": "articles",
+    "edit": "edit"
+} as const;
+
+export const CONTENT_TYPE = {
+    "anime": "Аніме",
+    "character": "Персонаж",
+    "person": "Автор",
+    "edit": "Правка",
+    "comment": "Коментар",
+    "collection": "Колекція",
+    "manga": "Манґа",
+    "novel": "Ранобе",
+    "user": "Користувач",
+    "article": "Стаття"
+} as const;
+
+export const HIKKA_BASE = "https://hikka.io" as const;
--- src/modules/user-comments/UserCommentComponent.tsx
+++ src/modules/user-comments/UserCommentComponent.tsx
@@ -3,16 +3,20 @@ import { useUserComments } from '@/hooks/useUserComments';
 import { Card } from '@/components/ui/card';
 import MDViewer from '@/components/ui/markdown/MD-viewer';
 import Link from '@/components/typography/link';
+import { CONTENT_TYPE, CONTENT_TYPE_URL, HIKKA_BASE } from '@/constants';
+import { Badge } from '@/components/ui/badge';
 
 interface UserCommentComponentProps {
   username: string;
   commentsPerPage?: number;
 }
 
-const getTypeOfUrl = (url: string): string => {
-  const urlParts = url.split("/");
-  const typeOfAnime = urlParts[3];
-  return typeOfAnime.slice(0, 1).toUpperCase() + typeOfAnime.slice(1).toLowerCase();
+const getSourceURL = (sourceType: string, sourceSlug: string): string => {
+  return `${HIKKA_BASE}/comments/${CONTENT_TYPE_URL[sourceType as keyof typeof CONTENT_TYPE_URL]}/${sourceSlug}`;
+};
+
+const getSourceName = (sourceType: string): string => {
+  return `${CONTENT_TYPE[sourceType as keyof typeof CONTENT_TYPE]}`;
 };
 
 const UserCommentComponent: React.FC<UserCommentComponentProps> = () => {
@@ -20,41 +24,44 @@ const UserCommentComponent: React.FC<UserCommentComponentProps> = () => {
   const { data, loading, error, refresh, } = useUserComments({ username: username });
   const croppedComments = data?.comments.slice(0, 3) || [];
 
-  if (error) { return null; }
+  if (error) {
+    return null;
+  }
 
   if (loading) {
-    return (<>
+    return (
       <div className="flex flex-row w-full gap-4">
-        {Array.from({ length: 3 }).map((_) => (
-          <Card className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate gap-6 overflow-hidden rounded-none md:rounded-lg">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Card key={index} className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate gap-6 overflow-hidden rounded-none md:rounded-lg">
             <div className="flex flex-col gap-2 w-full">
-              <div className="w-64 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-              <div className="w-64 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-              <div className="w-64 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+              <div className="w-full h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+              <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+              <div className="w-4/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+              <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
             </div>
-            <div className="w-24 h-2 animate-pulse bg-secondary/20 rounded-lg"></div>
+            <div className="w-24 h-2 animate-pulse bg-secondary/20 rounded-lg mt-4"></div>
           </Card>
         ))}
       </div>
-    </>)
+    );
   }
 
   return (
-    <>
-      <div className="flex flex-row w-full gap-4">
-        {croppedComments.map((comment) => (
-          <Card className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate rounded-none md:rounded-lg justify-between">
-            <MDViewer className="text-[0.9375rem] line-clamp-4">
-                {comment.text}
-              </MDViewer>
-            <Link href={comment.source} className="text-xs leading-normal line-clamp-1 mt-2">
-              {getTypeOfUrl(comment.source)}
-            </Link>
-          </Card>
-        ))}
-      </div>
-    </>
-  )
+    <div className="flex flex-row w-full gap-4">
+      {croppedComments.map((comment, index) => (
+        <Card key={index} className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate rounded-none rounded-lg justify-between">
+          <MDViewer className="text-[0.9375rem] line-clamp-4">
+            {comment.text}
+          </MDViewer>
+          <Link href={getSourceURL(comment.source_type, comment.source_slug)}>
+            <Badge variant="secondary" className="rounded-lg cursor-pointer">
+              {getSourceName(comment.source_type)}
+            </Badge>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default UserCommentComponent;

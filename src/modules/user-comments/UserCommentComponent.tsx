f55d410aff8d578d71c301918cccbd01f676c330
Lorg0n <lorgon.kv@gmail.com>
2025-05-21 23:31:50 +0300

    feat: make user comments block more cool

--- src/modules/user-comments/UserCommentComponent.tsx
+++ src/modules/user-comments/UserCommentComponent.tsx
@@ -5,6 +5,10 @@ import MDViewer from '@/components/ui/markdown/MD-viewer';
 import Link from '@/components/typography/link';
 import { CONTENT_TYPE, CONTENT_TYPE_URL, HIKKA_BASE } from '@/constants';
 import { Badge } from '@/components/ui/badge';
+import NotFound from '@/components/ui/not-found';
+import { Button } from '@/components/ui/button';
+import { Icon } from '@iconify/react';
+
 
 interface UserCommentComponentProps {
   username: string;
@@ -21,45 +25,80 @@ const getSourceName = (sourceType: string): string => {
 
 const UserCommentComponent: React.FC<UserCommentComponentProps> = () => {
   const username = window.location.pathname.split('/u/')[1];
-  const { data, loading, error, refresh, } = useUserComments({ username: username });
+  const { data, loading, error, } = useUserComments({ username: username });
   const croppedComments = data?.comments.slice(0, 3) || [];
-
-  if (error) {
-    return null;
-  }
 
   if (loading) {
     return (
-      <div className="flex flex-row w-full gap-4">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <Card key={index} className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate gap-6 overflow-hidden rounded-none md:rounded-lg">
-            <div className="flex flex-col gap-2 w-full">
-              <div className="w-full h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-              <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-              <div className="w-4/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-              <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
-            </div>
-            <div className="w-24 h-2 animate-pulse bg-secondary/20 rounded-lg mt-4"></div>
-          </Card>
-        ))}
+      <div className="flex flex-col gap-8">
+        <h3 className="font-display text-xl font-bold">Коментарі</h3>
+        <div className="flex flex-row w-full gap-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Card key={index} className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate gap-6 overflow-hidden rounded-none md:rounded-lg">
+              <div className="flex flex-col gap-2 w-full">
+                <div className="w-full h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+                <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+                <div className="w-4/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+                <div className="w-5/6 h-3 animate-pulse bg-secondary/20 rounded-lg"></div>
+              </div>
+              <div className="w-20 h-3 animate-pulse bg-secondary/20 rounded-lg mt-4"></div>
+            </Card>
+          ))}
+        </div>
       </div>
     );
   }
 
+  if (croppedComments.length === 0 || error) {
+    return (
+      <div className="flex flex-col gap-8">
+        <h3 className="font-display text-xl font-bold">Коментарі</h3>
+        <NotFound
+          title="Коментарів поки немає"
+          description="Можливо, вони ще не оновилися або їх ще ніхто не залишив"
+        />
+      </div>
+    );
+  }
+
+
   return (
-    <div className="flex flex-row w-full gap-4">
-      {croppedComments.map((comment, index) => (
-        <Card key={index} className="flex flex-col w-1/3 bg-secondary/20 p-4 isolate rounded-none rounded-lg justify-between">
-          <MDViewer className="text-[0.9375rem] line-clamp-4">
-            {comment.text}
-          </MDViewer>
-          <Link href={getSourceURL(comment.source_type, comment.source_slug)}>
-            <Badge variant="secondary" className="rounded-lg cursor-pointer">
-              {getSourceName(comment.source_type)}
-            </Badge>
-          </Link>
-        </Card>
-      ))}
+    <div className="flex flex-col gap-8">
+      <h3 className="font-display text-xl font-bold">Коментарі</h3>
+      <div className="flex flex-col md:flex-row w-full gap-4">
+        {croppedComments.map((comment, index) => (
+          <Card key={index} className="flex flex-col md:w-1/3 bg-secondary/20 p-4 isolate rounded-none rounded-lg justify-between">
+            <MDViewer className="text-[0.9375rem] line-clamp-4">
+              {comment.text}
+            </MDViewer>
+            <div className="flex flex-row justify-between items-center w-full">
+              <Link href={getSourceURL(comment.source_type, comment.source_slug)}>
+                <Badge variant="secondary" className="rounded-lg cursor-pointer">
+                  {getSourceName(comment.source_type)}
+                </Badge>
+              </Link>
+              <div className="flex flex-row gap-0">
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    className="pointer-events-none gap-1 text-muted-foreground"
+                  >
+                    <Icon icon="tabler:message-filled" className="size-3" />
+                    {comment.total_replies}
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="pointer-events-none gap-1 text-muted-foreground"
+                  >
+                    <Icon icon="bxs:upvote" className="size-3" />
+                    {comment.vote_score}
+                  </Button>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,13 +2,17 @@ export interface CommentItem {
     text: string;
     source_type: string;
     source_slug: string;
+    vote_score: number;
+    total_replies: number;
 }
 
 export interface UserCommentsApiResponse {
     comments: CommentItem[];
     author_username: string;
-    author_reference: string; // UUID
+    author_reference: string;
     author_avatar: string; // URL
+    vote_score: number;
+    total_replies: number;
 }
 
 export interface ApiErrorResponse {

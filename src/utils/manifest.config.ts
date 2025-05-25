f1efa0f1b8bc9aa161fc732829b2812cbf5da866
Lorg0n <lorgon.kv@gmail.com>
2025-05-25 04:38:10 +0300

    chore: set current version

--- src/utils/manifest.config.ts
+++ src/utils/manifest.config.ts
@@ -1,7 +1,7 @@
 const getBaseManifest = () => ({
     manifest_version: 3,
     name: "Hikka Forge",
-    version: "0.2.1", 
+    version: "null", 
     description: "Unlock custom features and personalized styles for Hikka.io with this extension.",
     action: {
         default_popup: "src/pages/popup/popup.html",

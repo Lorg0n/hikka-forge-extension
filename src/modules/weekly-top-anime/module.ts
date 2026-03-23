be6f1453cef95f76143090c131b125bb9f200dd8
Lorg0n <lorgon.kv@gmail.com>
2026-03-23 23:12:24 +0300

    fix: update weekly ongoings top selector

--- src/modules/weekly-top-anime/module.ts
+++ src/modules/weekly-top-anime/module.ts
@@ -8,7 +8,7 @@ const weeklyTopAnimeModule: ForgeModuleDef = {
     urlPatterns: ['https://hikka.io/'],
     enabledByDefault: true,
     elementSelector: {
-        selector: 'div:has(> a[href="/anime?statuses=ongoing"] > h2.scroll-m-20)',
+        selector: 'div:has(> a[href="/anime?statuses=%5B%22ongoing%22%5D&types=%5B%22tv%22%5D&seasons=%5B%22winter%22%5D&years=%5B2026%2C2026%5D&sort=%5B%22scored_by%22%2C%22score%22%2C%22native_scored_by%22%2C%22native_score%22%5D&order=desc"] > h4)',
         position: 'append',
     },
     component: WeeklyTopAnimeComponent
